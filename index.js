157956d57da0d9da8d8ca12e5eee51cbf30d9cbd
nusratjahan7 <nusratjahan77222@gmail.com>
2026-05-18 09:31:40 +0600

    database setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+const cors = require('cors');
+dotenv.config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db('arenahub');
+        const destinationCollection = db.collection('facilities');
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Serving')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
