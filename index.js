f64c2cd9e0f5212f876f2e5c4264d746c20e6d6d
nusratjahan7 <nusratjahan77222@gmail.com>
2026-05-18 20:24:25 +0600

    comment client for deploy in vercel

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const db = client.db('arenahub');
         const facilitiesCollection = db.collection('facilities');
         const bookingCollection = db.collection('bookings');
@@ -123,7 +123,7 @@ async function run() {
         })
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
